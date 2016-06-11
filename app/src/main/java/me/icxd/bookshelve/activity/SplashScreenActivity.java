adc03835f8aa64b11155a476b41f867682d80d5d
HaPBoy <ihapboy@gmail.com>
2016-06-11 15:59:00 +0800

    添加启动画面

---
+++ app/src/main/java/me/icxd/bookshelve/activity/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package me.icxd.bookshelve.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import me.icxd.bookshelve.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN ,WindowManager.LayoutParams. FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, 2000);
+    }
+}
